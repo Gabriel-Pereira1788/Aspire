66b2654348aecab6b40666a5be9fad2f86659e95
Gabriel Ploomes <gabriel.andrade@ploomes.com>
2024-11-19 22:56:36 -0300

    Initial Commit

---
+++ Aspire/AspireApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspireApp.swift
+//  Aspire
+//
+//  Created by Gabriel Pereira on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AspireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
